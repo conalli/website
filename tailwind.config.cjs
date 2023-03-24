784b2383c8f3bcf5d6cf22db552c0621d6d3d3c0
Conall <conallimlah@gmail.com>
2023-03-24 20:22:21 +0900

    update packages, tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [],
 };
