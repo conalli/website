3b373a0a917e6881e54e42398444a5ce351eeab3
Conall <conallimlah@gmail.com>
2023-03-25 23:04:22 +0900

    update nav, header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,19 @@
-import { motion, Variants } from "framer-motion";
+import { CustomView, isMobileOnly } from "react-device-detect";
 import { useWindowDimensions } from "../hooks/useWindowDimensions";
 import MobileNav from "./MobileNav";
 import PageLinks from "./PageLinks";
 
-const MOBILE_MAX_WIDTH = 500;
-
-const controls: Variants = {
-  none: {
-    opacity: 1,
-  },
-  init: {
-    opacity: 0,
-    y: 10,
-  },
-  anim: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 1 },
-  },
-};
-
 function Nav({ currentUrl }: { currentUrl: string }) {
   const { width } = useWindowDimensions();
-  if (width === 0) return null;
   return (
-    <motion.nav variants={controls} initial={"init"} animate={"anim"}>
-      {width < MOBILE_MAX_WIDTH ? (
-        <MobileNav currentUrl={currentUrl} />
-      ) : (
+    <nav>
+      <CustomView condition={width > 500}>
         <PageLinks currentUrl={currentUrl} />
-      )}
-    </motion.nav>
+      </CustomView>
+      <CustomView condition={isMobileOnly || width <= 500}>
+        <MobileNav currentUrl={currentUrl} />
+      </CustomView>
+    </nav>
   );
 }
 
