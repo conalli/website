a3204564a6de8ef2f342be27e32faa2fab4ee051
Conall <conallimlah@gmail.com>
2023-03-26 18:58:26 +0900

    update configs

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,14 +2,27 @@ import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
   schema: z.object({
+    draft: z.boolean().default(false),
     title: z.string(),
     description: z.string(),
-    publishDate: z.date(),
+    publishDate: z.date().transform((str) => new Date(str)),
     image: z.string().optional(),
-    tags: z.array(z.string()),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean().default(false),
+    title: z.string(),
+    description: z.string(),
+    logo: z.string().optional(),
+    video: z.string().url().optional(),
+    image: z.string().optional(),
   }),
 });
 
 export const collections = {
   blog: blogCollection,
+  projects: projectCollection,
 };
