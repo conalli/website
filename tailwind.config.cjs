d2913f7c138154ea720765cd25b850eab1e318d4
Conall <conallimlah@gmail.com>
2023-03-27 15:48:31 +0900

    add custom animation to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
       screens: {
         xs: "480px",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.4s ease-in",
+      },
     },
   },
   plugins: [],
