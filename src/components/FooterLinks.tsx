475fa6f93029236ccd39a1d07198fdce493d7f9a
Conall <conallimlah@gmail.com>
2023-03-29 22:18:11 +0900

    update footer + header components

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -1,22 +1,45 @@
 import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
-const FooterLinks = () => {
+import { motion } from "framer-motion";
+
+const hoverScale = { scale: 1.1 };
+const tapScale = { scale: 0.9 };
+
+function FooterLinks() {
   return (
-    <ul className="flex w-full flex-even">
-      <li>
+    <motion.ul
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 2 } }}
+      className="flex w-full justify-evenly items-center text-lg text-slate-500"
+    >
+      <motion.li
+        whileHover={hoverScale}
+        whileTap={tapScale}
+        className="hover:text-white"
+      >
+        <a href="/about#contact" className="font-serif">
+          contact<span className="text-blue-500">.</span>
+        </a>
+      </motion.li>
+      <motion.li
+        whileHover={hoverScale}
+        whileTap={tapScale}
+        className="hover:text-white"
+      >
         <a href="https://github.com/conalli">
-          <GitHubLogoIcon />
+          <GitHubLogoIcon width={22} height={22} />
         </a>
-      </li>
-      <li>
+      </motion.li>
+      <motion.li
+        whileHover={hoverScale}
+        whileTap={tapScale}
+        className="hover:text-blue-500"
+      >
         <a href="https://linkedin.com/in/conalli">
-          <LinkedInLogoIcon />
+          <LinkedInLogoIcon width={22} height={22} />
         </a>
-      </li>
-      <li>
-        <a href="/about#contact">contact me</a>
-      </li>
-    </ul>
+      </motion.li>
+    </motion.ul>
   );
-};
+}
 
 export default FooterLinks;
