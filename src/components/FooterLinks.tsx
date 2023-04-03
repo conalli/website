b6a73c9a74dcbc14871de0f9b4f9187db3f0c9f9
Conall <conallimlah@gmail.com>
2023-04-03 22:16:57 +0900

    update footer styling

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -9,33 +9,43 @@ function FooterLinks() {
     <motion.ul
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 2 } }}
-      className="flex w-full justify-evenly items-center text-lg text-slate-500"
+      className="flex w-full justify-evenly items-center text-lg text-slate-500 md:w-1/2 mx-auto"
     >
       <motion.li
         whileHover={hoverScale}
         whileTap={tapScale}
-        className="hover:text-white"
+        className="hover:text-white w-1/4"
       >
-        <a href="/about#contact" className="font-serif">
+        <a href="/about#contact" className="font-serif flex justify-center">
           contact<span className="text-blue-500">.</span>
         </a>
       </motion.li>
       <motion.li
         whileHover={hoverScale}
         whileTap={tapScale}
-        className="hover:text-white"
+        className="hover:text-white w-1/4"
       >
-        <a href="https://github.com/conalli">
+        <a
+          href="https://github.com/conalli"
+          className="flex justify-center"
+          title="Github"
+        >
           <GitHubLogoIcon width={22} height={22} />
+          <span className="hidden">Github</span>
         </a>
       </motion.li>
       <motion.li
         whileHover={hoverScale}
         whileTap={tapScale}
-        className="hover:text-blue-500"
+        className="hover:text-blue-500 w-1/4"
       >
-        <a href="https://linkedin.com/in/conalli">
+        <a
+          href="https://linkedin.com/in/conalli"
+          className="flex justify-center"
+          title="LinkedIn"
+        >
           <LinkedInLogoIcon width={22} height={22} />
+          <span className="hidden">LinkedIn</span>
         </a>
       </motion.li>
     </motion.ul>
