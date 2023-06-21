065b8a8a06df36564d94c592785475b3baa07ce4
Conall <conallimlah@gmail.com>
2023-06-22 07:40:50 +0900

    temporarily remove 3d model

--- src/components/3d/Scene.tsx
+++ src/components/3d/Scene.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 import { useStore } from "@nanostores/react";
-import { OrbitControls, Stage } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { EffectComposer, Vignette } from "@react-three/postprocessing";
 import { AnimatePresence, motion } from "framer-motion";
 import { PropsWithChildren, Suspense } from "react";
 import { useMousePosition, useWindowDimensions } from "src/hooks";
 import { loadingAtom } from "src/store/loading";
 import { DEG2RAD } from "three/src/math/MathUtils";
 import Loader from "./Loader";
-import { PC, Plant } from "./models";
 
 function Scene({ children }: PropsWithChildren) {
   const loading = useStore(loadingAtom);
@@ -50,7 +48,7 @@ const HomeScene = () => {
     <Scene>
       <ambientLight intensity={0.5} />
       <directionalLight color="red" position={[0, 10, 0]} intensity={0.5} />
-      <Stage
+      {/* <Stage
         intensity={0.1}
         preset="rembrandt"
         shadows={{
@@ -67,7 +65,7 @@ const HomeScene = () => {
         <EffectComposer>
           <Vignette eskil={false} offset={0.1} darkness={0.8} />
         </EffectComposer>
-      </Stage>
+      </Stage> */}
       <OrbitControls
         minPolarAngle={Math.PI / 4}
         maxPolarAngle={Math.PI / 2}
