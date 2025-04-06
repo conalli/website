e095c099815ea6e92234819d4eefa91aa556b21b
Conall <conallimlah@gmail.com>
2025-04-06 19:57:09 +0900

    remove middleware, add manual redirect

--- src/middleware/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineMiddleware } from "astro:middleware";
-
-export const onRequest = defineMiddleware((context, next) => {
-  if (context.url.pathname !== "/under-construction") {
-    return context.redirect("/under-construction");
-  }
-  return next();
-});
